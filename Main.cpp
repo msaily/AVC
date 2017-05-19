d0b55d86a778251591b7dc3764c2b6d0426d5337
michealsaily <micheal.msaily@gmail.com>
2017-05-19 14:32:13 +1200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,7 +8,7 @@
 
 #include <stdio.h>
 #include <time.h>
-
+// If the sum is equal to 999 - then its on the black 
 int read_camera(){
     double col = 0;
     double row = 120;
@@ -16,16 +16,14 @@ int read_camera(){
     int sum, s = 0;
     double signal = 0;
     int white = 0;
+    int numWhitePixels = 0;
     
     for(int i = 0; i < 320; i++){
-        white = get_pixel(col, row, 3);
-        if(white > 127){
-            s = 1;
-        }
-        else {
-            s = 0;
-        }
+        s = (get_pixel(col, i, 3) > 127) ? 1:0;
+        numWhitePixels = (s==1) ? numWhitePixels++:numWhitePixels;
         sum = sum + (i-160)*s;
+        if(numWhitePixels > 0){ sum/numWhitePixels; }
+        else { sum = 999; }
     }
     
     signal = sum;
@@ -37,9 +35,9 @@ int main(){
     double errorPID;
     double signal = 0;
     
-    int driveLeft = 100;
-    int driveRight = 100;
-    
+    int driveLeft, driveRight = 50;
+    int backSpeed = -127;
+  
     // Implementing PID
     
     float dAdjustment = 0.5;
@@ -61,6 +59,13 @@ int main(){
         }
         else if (singal > 0){
             set_motor(1, driveRight);
+            //set_motor(0, driveLeft);
+        }
+        else if (singal == 999){
+            driveLeft = -127;
+            driveRight = -127;
+            set_motor(1, backSpeed);
+            set_motor(0, backSpeed)
             //set_motor(0, driveLeft);
         }
         else {
